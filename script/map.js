eee57d4a281da698c40c2a81d8d3a931b175f718
unknown <jschwab@NB-TT-118113.hsr.ch>
2015-09-18 11:19:15 +0200

    Fixed the Leaflet-Map

--- script/map.js
+++ script/map.js
@@ -1,8 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([46.8259, 8.2000], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy;<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'your.mapbox.project.id',
-    accessToken: 'your.mapbox.public.access.token'
+L.tileLayer('http://tile.osm.ch/osm-swiss-style/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy;<a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 18
 }).addTo(map);
