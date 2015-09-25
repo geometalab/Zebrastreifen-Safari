6f83b69f6ded992c5dc95b4215794d0d696c7960
unknown <jschwab@NB-TT-118113.hsr.ch>
2015-09-25 10:34:28 +0200

    Changed coloring Added bounds adjusted website for mobile

--- script/linechart.js
+++ script/linechart.js
@@ -6,11 +6,13 @@ var ctx = document.getElementById("linecanvas").getContext("2d");
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
             data: [65, 59, 80, 81, 56, 55, 40]
         }
     ]
--- script/map.js
+++ script/map.js
@@ -1,6 +1,15 @@
-var map = L.map('map').setView([46.8259, 8.2000], 13);
+
+var southWest = L.latLng(45.7300, 5.8000),
+    northEast = L.latLng(47.9000, 10.600),
+    bounds = L.latLngBounds(southWest, northEast);
+
+var map = L.map('map',{
+    maxBounds:bounds
+}).setView([46.8259, 8.2000], 9);
+
+
 
 L.tileLayer('http://tile.osm.ch/osm-swiss-style/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy;<a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    maxZoom: 18
+    maxZoom: 18,
 }).addTo(map);
