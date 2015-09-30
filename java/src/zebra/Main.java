4058a31215ba0a1b4290e8888cb9abfe9562f015
unknown <aeugster@NB-TT-117912.hsr.ch>
2015-09-30 15:41:53 +0200

    initial commit for java

---
+++ java/src/zebra/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zebra;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aeugster
+ */
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("Alex", "AE"));
+        users.add(new User("Joël", "JS"));
+        users.add(new User("Stefan", "SK"));
+        users.add(new User("Mike", "MM"));
+        users.add(new User("Fabienne", "FK"));
+        
+        GUI gui = new GUI(users);
+        gui.setVisible(true);
+        
+    }
+}
