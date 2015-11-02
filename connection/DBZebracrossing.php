1c667b3bd130ab419a13fafcab21730ace1fc0a3
Mike Marti <mike-marti@outlook.com>
2015-11-02 09:00:01 +0100

    Revert "Changed sql statements for new db structure"
    This reverts commit 8aae6ad7a6dcd6b79d7afa41fadcc98c25f94c98.

--- connection/DBZebracrossing.php
+++ connection/DBZebracrossing.php
@@ -13,40 +13,40 @@ class DBZebracrossing extends DBConnection {
     }
 
     public function getZebracrossing($osm_node_id) {
-        return pg_query($this->connection, "select zebra.id, zebra.osm_node_id, zebra.status from crossing as zebra
+        return pg_query($this->connection, "select zebra.id, zebra.osm_node_id, zebra.status from crossing.crossing as zebra
                                             where zebra.osm_node_id = $osm_node_id;");
     }
 
     public function getAllZebracrossings() {
-        return pg_query($this->connection, "select zebra.osm_node_id, zebra.status from crossing as zebra
+        return pg_query($this->connection, "select zebra.osm_node_id, zebra.status from crossing.crossing as zebra
                                             order by id;");
     }
 
     public function getRating($id) {
-        return pg_query($this->connection, "select sc.value as sc_value, i.value as i_value, t.value as t_value, rating.image_weblink, rating.comment, u.name from rating as rating
-                                            inner join crossinguser as u
+        return pg_query($this->connection, "select sc.value as sc_value, i.value as i_value, t.value as t_value, rating.image_weblink, rating.comment, u.name from crossing.rating as rating
+                                            inner join crossing.user as u
                                             on user_id = u.id
-                                            inner join spatial_clarity as sc
+                                            inner join crossing.spatial_clarity as sc
                                             on spatial_clarity_id = sc.id
-                                            inner join illumination as i
+                                            inner join crossing.illumination as i
                                             on illumination_id = i.id
-                                            inner join traffic as t
+                                            inner join crossing.traffic as t
                                             on traffic_id = t.id
                                             where crossing_id = $id;");
     }
 
     public function getLineChartStatistic() {
         //TODO: not the sum of the amount! ask for implementation to get the last available row of the week
-        return pg_query($this->connection, "select date_trunc('week', enquiry_date) as week, max(amount) as amount from crossingstatistic
+        return pg_query($this->connection, "select date_trunc('week', enquiry_date) as week, max(amount) as amount from statistic.crossingstatistic
                                             group by week
                                             order by week asc;");
     }
 
     public function getBarChartStatistic() {
         //TODO: not the sum of the amount! ask for implementation to get the last available row of the week
-        return pg_query($this->connection, "select date_trunc('week', enquiry_date) as week, max(amount) as amount from crossingstatistic
+        return pg_query($this->connection, "select date_trunc('week', enquiry_date) as week, max(amount) as amount from statistic.crossingstatistic
                                             group by week
                                             order by week asc
-                                            offset (select count(distinct date_trunc('week', enquiry_date)) from crossingstatistic) - 11;");
+                                            offset (select count(distinct date_trunc('week', enquiry_date)) from statistic.crossingstatistic) - 11;");
     }
 }
