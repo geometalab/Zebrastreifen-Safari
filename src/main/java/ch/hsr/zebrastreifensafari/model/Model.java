78de9d8b9bdea22bd45e4c60d0c3cc275d552adc
Fabienne König <fkoenig@hsr.ch>
2015-11-04 14:18:21 +0100

    sort functionality for ratingDataTable added

--- src/main/java/ch/hsr/zebrastreifensafari/model/Model.java
+++ src/main/java/ch/hsr/zebrastreifensafari/model/Model.java
@@ -25,7 +25,7 @@ public class Model {
     private List<Illumination> illuminations;
     private List<SpatialClarity> spatialClaritys;
     private List<Traffic> traffics;
-    private boolean node = true, ratingAmount = true;
+    private boolean node = true, ratingAmount = true, user = true, traffic = true, clarity = true, illumination = true, comment = true, image = true, lastChanged = true;
 
     public Model() {
         ratingMode = false;
@@ -156,7 +156,7 @@ public class Model {
     
     public void sortByNode() {
     	if (node) {
-            crossings = crossings.stream().sorted((o1, o2) ->  Long.compare(o1.getOsmNodeId(), o2.getOsmNodeId())).collect(Collectors.toList());
+            crossings = crossings.stream().sorted((o1, o2) ->  Long.compare(o1.getOsmNodeId(), o2.getOsmNodeId())).collect(Collectors.toList());          
     		node = false;
     	}
     	else {
@@ -177,6 +177,79 @@ public class Model {
     }
 
     public void sortByUser() {
-        ratings = ratings.stream().sorted(((o1, o2) -> o1.getUserId().getName().compareTo(o2.getUserId().getName()))).collect(Collectors.toList());
+    	if (user) {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getUserId().getName().compareTo(o2.getUserId().getName()))).collect(Collectors.toList());
+    		user = false;
+    	}
+    	else {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getUserId().getName().compareTo(o1.getUserId().getName()))).collect(Collectors.toList());
+    		user = true;
+    	}
+    }
+    
+    public void sortByTraffic() {
+    	if (traffic) {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getTrafficId().getValue().compareTo(o2.getTrafficId().getValue()))).collect(Collectors.toList());
+    		traffic = false;
+    	}
+    	else {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getTrafficId().getValue().compareTo(o1.getTrafficId().getValue()))).collect(Collectors.toList());
+    		traffic = true;
+    	}
+    }
+    
+    public void sortByClarity() {
+    	if (clarity) {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getSpatialClarityId().getValue().compareTo(o2.getSpatialClarityId().getValue()))).collect(Collectors.toList());
+    		clarity = false;
+    	}
+    	else {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getSpatialClarityId().getValue().compareTo(o1.getSpatialClarityId().getValue()))).collect(Collectors.toList());
+    		clarity = true;
+    	}
+    }
+    
+    public void sortByIllumination() {
+    	if (illumination) {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getIlluminationId().getValue().compareTo(o2.getIlluminationId().getValue()))).collect(Collectors.toList());
+    		illumination = false;
+    	}
+    	else {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getIlluminationId().getValue().compareTo(o1.getIlluminationId().getValue()))).collect(Collectors.toList());
+    		illumination = true;
+    	}
+    }
+    
+    public void sortByComment() {
+    	if (comment) {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getComment().compareTo(o2.getComment()))).collect(Collectors.toList());
+    		comment = false;
+    	}
+    	else {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getComment().compareTo(o1.getComment()))).collect(Collectors.toList());
+    		comment = true;
+    	}
+    }
+    
+    public void sortByImage() {
+    	if (image) {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getImageWeblink().compareTo(o2.getImageWeblink()))).collect(Collectors.toList());
+    		image = false;
+    	}
+    	else {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getImageWeblink().compareTo(o1.getImageWeblink()))).collect(Collectors.toList());
+    		image = true;
+    	}
+    }
+    
+    public void sortByLastChanged() {
+    	if (lastChanged) {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getLastChanged().compareTo(o2.getLastChanged()))).collect(Collectors.toList());
+    		lastChanged = false;
+    	}
+    	else {
+    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getLastChanged().compareTo(o1.getLastChanged()))).collect(Collectors.toList());
+    		lastChanged = true;
+    	}
     }
 }
