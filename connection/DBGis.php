067ec937de3322e74fcb4045b1a4c041c0cf9b51
Mike Marti <mike-marti@outlook.com>
2015-11-16 15:47:43 +0100

    Implemented new queries

--- connection/DBGis.php
+++ connection/DBGis.php
@@ -18,38 +18,52 @@ class DBGis extends DBConnection {
     }
 
     public function getGisData($osm_node_id) {
-        return pg_query($this->connection, "select
-                                            tags @> hstore('crossing', 'traffic_signals') as traffic_signals,
-                                            tags @> hstore('crossing', 'island') as island,
-                                            tags @> hstore('crossing', 'unmarked') as unmarked,
-                                            tags @> hstore('button_operated', 'yes') as button_operated,
-                                            tags @> hstore('tactile_paving', 'yes') as tactile_paving,
-                                            tags @> hstore('traffic_signals:vibration', 'yes') as traffic_signals_vibration,
-                                            tags @> hstore('traffic_signals:sound', 'yes') as traffic_signals_sound
-                                            from osm_point
-                                            where tags @> hstore('highway', 'crossing')
-                                            and osm_id = $osm_node_id;");
+        return pg_query($this->connection, "SELECT
+                                            tags @> hstore('crossing', 'traffic_signals') AS traffic_signals,
+                                            tags @> hstore('crossing', 'island') AS island,
+                                            tags @> hstore('crossing', 'unmarked') AS unmarked,
+                                            tags @> hstore('button_operated', 'yes') AS button_operated,
+                                            tags @> hstore('tactile_paving', 'yes') AS tactile_paving,
+                                            tags @> hstore('traffic_signals:vibration', 'yes') AS traffic_signals_vibration,
+                                            tags @> hstore('traffic_signals:sound', 'yes') AS traffic_signals_sound
+                                            FROM osm_point
+                                            WHERE tags @> hstore('highway', 'crossing')
+                                            AND osm_id = $osm_node_id;");
     }
 
     public function getCoordinates($osm_node_id) {
-        return pg_query($this->connection, "select ST_X(way) as x, ST_Y(way) as y
-                                            from osm_point
-                                            where tags @> hstore('highway', 'crossing')
-                                            and osm_id = $osm_node_id;");
+        return pg_query($this->connection, "SELECT ST_X(way) AS x, ST_Y(way) AS y
+                                            FROM osm_point
+                                            WHERE tags @> hstore('highway', 'crossing')
+                                            AND osm_id = $osm_node_id;");
     }
 
     public function getState($tag, $value, $osm_node_id) {
-        return pg_query($this->connection, "select tags @> hstore('$tag', '$value')
-                                            from osm_point
-                                            where tags @> hstore('highway', 'crossing')
-                                            and tags @> hstore('$tag', '$value')
-                                            and osm_id = $osm_node_id;");
+        return pg_query($this->connection, "SELECT tags @> hstore('$tag', '$value')
+                                            FROM osm_point
+                                            WHERE tags @> hstore('highway', 'crossing')
+                                            AND tags @> hstore('$tag', '$value')
+                                            AND osm_id = $osm_node_id;");
     }
 
     public function getYesNoState($tag, $osm_node_id) {
-        return pg_query($this->connection, "select tags @> hstore('$tag', 'yes') as state
-                                            from osm_point
-                                            where tags @> hstore('highway', 'crossing')
-                                            and osm_id = $osm_node_id;");
+        return pg_query($this->connection, "SELECT tags @> hstore('$tag', 'yes') AS state
+                                            FROM osm_point
+                                            WHERE tags @> hstore('highway', 'crossing')
+                                            AND osm_id = $osm_node_id;");
+    }
+
+    public function getCrossingAmount() {
+        return pg_query($this->connection, "SELECT COUNT(*) AS amount
+                                            FROM osm_point
+                                            WHERE tags @> hstore('highway', 'crossing')");
+    }
+
+    public function getClusteredAmount($number) {
+        return pg_query($this->connection, "SELECT COUNT(*) FROM
+                                            (SELECT COUNT(way) FROM osm_point
+                                            WHERE tags @> hstore('highway','crossing')
+                                            GROUP BY ST_SnapToGrid(way, $number))
+                                            AS tmp;");
     }
 }
