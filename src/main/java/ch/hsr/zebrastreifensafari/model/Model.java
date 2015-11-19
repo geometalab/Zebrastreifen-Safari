e7cd677792f895205eb4360b93eb28625b1f374f
Alex <iamalexeugster@gmail.com>
2015-11-19 08:38:15 +0100

    some small changes

--- src/main/java/ch/hsr/zebrastreifensafari/model/Model.java
+++ src/main/java/ch/hsr/zebrastreifensafari/model/Model.java
@@ -155,74 +155,68 @@ public class Model {
 
     //<editor-fold desc="Sort methods">
     public void sortByNode() {
-    	if (node) {
-            crossings = crossings.stream().sorted((o1, o2) ->  Long.compare(o1.getOsmNodeId(), o2.getOsmNodeId())).collect(Collectors.toList());          
-    		node = false;
-    	}
-    	else {
-            crossings = crossings.stream().sorted((o1, o2) ->  Long.compare(o2.getOsmNodeId(), o1.getOsmNodeId())).collect(Collectors.toList());
-    		node = true;
-    	}
+        if (node) {
+            crossings = crossings.stream().sorted((o1, o2) -> Long.compare(o1.getOsmNodeId(), o2.getOsmNodeId())).collect(Collectors.toList());
+            node = false;
+        } else {
+            crossings = crossings.stream().sorted((o1, o2) -> Long.compare(o2.getOsmNodeId(), o1.getOsmNodeId())).collect(Collectors.toList());
+            node = true;
+        }
     }
-    
+
     public void sortByNumberOfRatings() {
-    	if (ratingAmount) {
-    		crossings = crossings.stream().sorted((o1, o2) ->  Long.compare(o1.getRatingAmount(), o2.getRatingAmount())).collect(Collectors.toList());
-    		ratingAmount = false;
-    	}
-    	else {
-            crossings = crossings.stream().sorted((o1, o2) ->  Long.compare(o2.getRatingAmount(), o1.getRatingAmount())).collect(Collectors.toList());
-    		ratingAmount = true;
-    	}
+        if (ratingAmount) {
+            crossings = crossings.stream().sorted((o1, o2) -> Long.compare(o1.getRatingAmount(), o2.getRatingAmount())).collect(Collectors.toList());
+            ratingAmount = false;
+        } else {
+            crossings = crossings.stream().sorted((o1, o2) -> Long.compare(o2.getRatingAmount(), o1.getRatingAmount())).collect(Collectors.toList());
+            ratingAmount = true;
+        }
     }
 
     public void sortByUser() {
-    	if (user) {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getUserId().getName().compareTo(o2.getUserId().getName()))).collect(Collectors.toList());
-    		user = false;
-    	}
-    	else {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getUserId().getName().compareTo(o1.getUserId().getName()))).collect(Collectors.toList());
-    		user = true;
-    	}
+        if (user) {
+            ratings = ratings.stream().sorted(((o1, o2) -> o1.getUserId().getName().compareTo(o2.getUserId().getName()))).collect(Collectors.toList());
+            user = false;
+        } else {
+            ratings = ratings.stream().sorted(((o1, o2) -> o2.getUserId().getName().compareTo(o1.getUserId().getName()))).collect(Collectors.toList());
+            user = true;
+        }
     }
-    
+
     public void sortByTraffic() {
-    	if (traffic) {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getTrafficId().getValue().compareTo(o2.getTrafficId().getValue()))).collect(Collectors.toList());
-    		traffic = false;
-    	}
-    	else {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getTrafficId().getValue().compareTo(o1.getTrafficId().getValue()))).collect(Collectors.toList());
-    		traffic = true;
-    	}
+        if (traffic) {
+            ratings = ratings.stream().sorted(((o1, o2) -> o1.getTrafficId().getValue().compareTo(o2.getTrafficId().getValue()))).collect(Collectors.toList());
+            traffic = false;
+        } else {
+            ratings = ratings.stream().sorted(((o1, o2) -> o2.getTrafficId().getValue().compareTo(o1.getTrafficId().getValue()))).collect(Collectors.toList());
+            traffic = true;
+        }
     }
-    
+
     public void sortByClarity() {
-    	if (clarity) {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getSpatialClarityId().getValue().compareTo(o2.getSpatialClarityId().getValue()))).collect(Collectors.toList());
-    		clarity = false;
-    	}
-    	else {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getSpatialClarityId().getValue().compareTo(o1.getSpatialClarityId().getValue()))).collect(Collectors.toList());
-    		clarity = true;
-    	}
+        if (clarity) {
+            ratings = ratings.stream().sorted(((o1, o2) -> o1.getSpatialClarityId().getValue().compareTo(o2.getSpatialClarityId().getValue()))).collect(Collectors.toList());
+            clarity = false;
+        } else {
+            ratings = ratings.stream().sorted(((o1, o2) -> o2.getSpatialClarityId().getValue().compareTo(o1.getSpatialClarityId().getValue()))).collect(Collectors.toList());
+            clarity = true;
+        }
     }
-    
+
     public void sortByIllumination() {
-    	if (illumination) {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getIlluminationId().getValue().compareTo(o2.getIlluminationId().getValue()))).collect(Collectors.toList());
-    		illumination = false;
-    	}
-    	else {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getIlluminationId().getValue().compareTo(o1.getIlluminationId().getValue()))).collect(Collectors.toList());
-    		illumination = true;
-    	}
+        if (illumination) {
+            ratings = ratings.stream().sorted(((o1, o2) -> o1.getIlluminationId().getValue().compareTo(o2.getIlluminationId().getValue()))).collect(Collectors.toList());
+            illumination = false;
+        } else {
+            ratings = ratings.stream().sorted(((o1, o2) -> o2.getIlluminationId().getValue().compareTo(o1.getIlluminationId().getValue()))).collect(Collectors.toList());
+            illumination = true;
+        }
     }
-    
+
     public void sortByComment() {
-    	if (comment) {
-    		ratings = ratings.stream().sorted(((o1, o2) -> {
+        if (comment) {
+            ratings = ratings.stream().sorted(((o1, o2) -> {
                 if (o1.getComment() == null) {
                     return 1;
                 } else if (o2.getComment() == null) {
@@ -231,10 +225,9 @@ public class Model {
                     return o1.getComment().compareTo(o2.getComment());
                 }
             })).collect(Collectors.toList());
-    		comment = false;
-    	}
-    	else {
-    		ratings = ratings.stream().sorted(((o1, o2) -> {
+            comment = false;
+        } else {
+            ratings = ratings.stream().sorted(((o1, o2) -> {
                 if (o1.getComment() == null) {
                     return -1;
                 } else if (o2.getComment() == null) {
@@ -243,13 +236,13 @@ public class Model {
                     return o2.getComment().compareTo(o1.getComment());
                 }
             })).collect(Collectors.toList());
-    		comment = true;
-    	}
+            comment = true;
+        }
     }
-    
+
     public void sortByImage() {
-    	if (image) {
-    		ratings = ratings.stream().sorted(((o1, o2) -> {
+        if (image) {
+            ratings = ratings.stream().sorted(((o1, o2) -> {
                 if (o1.getImageWeblink() == null) {
                     return 1;
                 } else if (o2.getImageWeblink() == null) {
@@ -258,10 +251,9 @@ public class Model {
                     return o1.getImageWeblink().compareTo(o2.getImageWeblink());
                 }
             })).collect(Collectors.toList());
-    		image = false;
-    	}
-    	else {
-    		ratings = ratings.stream().sorted(((o1, o2) -> {
+            image = false;
+        } else {
+            ratings = ratings.stream().sorted(((o1, o2) -> {
                 if (o1.getImageWeblink() == null) {
                     return -1;
                 } else if (o2.getImageWeblink() == null) {
@@ -270,19 +262,18 @@ public class Model {
                     return o2.getImageWeblink().compareTo(o1.getImageWeblink());
                 }
             })).collect(Collectors.toList());
-    		image = true;
-    	}
+            image = true;
+        }
     }
-    
+
     public void sortByLastChanged() {
-    	if (lastChanged) {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o1.getLastChanged().compareTo(o2.getLastChanged()))).collect(Collectors.toList());
-    		lastChanged = false;
-    	}
-    	else {
-    		ratings = ratings.stream().sorted(((o1, o2) -> o2.getLastChanged().compareTo(o1.getLastChanged()))).collect(Collectors.toList());
-    		lastChanged = true;
-    	}
+        if (lastChanged) {
+            ratings = ratings.stream().sorted(((o1, o2) -> o1.getLastChanged().compareTo(o2.getLastChanged()))).collect(Collectors.toList());
+            lastChanged = false;
+        } else {
+            ratings = ratings.stream().sorted(((o1, o2) -> o2.getLastChanged().compareTo(o1.getLastChanged()))).collect(Collectors.toList());
+            lastChanged = true;
+        }
     }
     //</editor-fold>
 }
