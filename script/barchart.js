6c25a6f07007afdc0b8f397307ff9bc66576d0c6
Nicola Jordan <nic@hixi.ch>
2018-04-18 11:21:41 +0200

    update charts

--- script/barchart.js
+++ script/barchart.js
@@ -1,70 +1,109 @@
 $(document).ready(function() {
-    var BASE_URL = 'https://zebrastreifen.sifs0003.infs.ch/zebra';
+  var BASE_URL = 'https://zebrastreifen.sifs0003.infs.ch/zebra';
 
-    $.ajax({
-        url: `${BASE_URL}/api/v1/crossingbarchart`,
+  var barChartOptions = {
+    responsive: true,
+    layout: {
+      padding: {
+        left: 20,
+        right: 20,
+        top: 0,
+        bottom: 0
+      }
+    },
+    title: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+      }],
+      yAxes: [{
+        display: true,
+        ticks: {
+          beginAtZero: true,
+          callback: function(value, index, values) {
+            return Number(value).toString() + " F* ";
+          },
+        },
+      }]
+    },
+    tooltips: {
+      callbacks: {
+        label: function(tooltipItem, data) {
+          var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+          if (label) {
+            label += ': ';
+          }
+          var value = tooltipItem.yLabel;
+          if (value === 0.3) {
+              value = "0";
+          }
+          label += value;
+          return label;
+        }
+      }
+    },
+  };
+
+  $.ajax({
+    url: `${BASE_URL}/api/v1/crossingbarchart`,
+    dataType: 'json',
+    method: 'get',
+    success: function (response)  {
+      var ctx = document.getElementById("barcanvas").getContext("2d");
+
+      var dataSaved = response;
+      var chartjsData = [];
+      var chartjsLabel = [];
+      for (var i = 0; i < dataSaved.length; i++) {
+        chartjsData.push(dataSaved[i].amount === 0 ? 0.3 : dataSaved[i].amount);
+        chartjsLabel.push(dataSaved[i].week);
+      }
+      $.ajax({
+        url: `${BASE_URL}/api/v1/ratingbarchart`,
         dataType: 'json',
         method: 'get',
-        success: function (response)  {
-            var ctx = document.getElementById("barcanvas").getContext("2d");
+        success: function (response2) {
+          var dataSaved2 = response2;
+          var ratingData = [];
+          for (var i = 0; i < dataSaved2.length; i++) {
+            ratingData.push(dataSaved2[i].amount === 0 ? 0.3 : dataSaved2[i].amount);
+          }
 
-            var dataSaved = response;
-            var chartjsData = [];
-            for (var i = 0; i < dataSaved.length; i++) {
-                chartjsData.push(dataSaved[i].amount);
-            }
-            var chartjsLabel = [];
-            for (var i = 0; i < dataSaved.length; i++) {
-                chartjsLabel.push(dataSaved[i].week);
-            }
-            $.ajax({
-                url: `${BASE_URL}/api/v1/ratingbarchart`,
-                dataType: 'json',
-                method: 'get',
-                success: function (response2) {
-                    var dataSaved2 = response2;
-                    var ratingData = [];
-                    for (var i = 0; i < dataSaved2.length; i++) {
-                        ratingData.push(dataSaved2[i].amount);
-                    }
+          var data = {
+            labels: chartjsLabel,
+            datasets: [
+              {
+                label: "Neu erfasste Fussgängerstreifen",
+                backgroundColor: "rgba(151,187,205,0.5)",
+                borderColor: "rgba(151,187,205,0.8)",
+                hoverBackgroundColor: "rgba(151,187,205,0.75)",
+                hoverBorderColor: "rgba(151,187,205,1)",
+                data: chartjsData
+              },
+              {
+                label: "bewertete Fussgängertreifen",
+                backgroundColor: "rgba(151,187,205,0.5)",
+                borderColor: "rgba(151,187,205,0.8)",
+                hoverBackgroundColor: "rgba(151,187,205,0.75)",
+                hoverBorderColor: "rgba(151,187,205,1)",
+                data: ratingData
+              },
+            ]
+          };
+          var myBarChart = new Chart(ctx, {
+            data: data,
+            type: 'bar',
+            options: barChartOptions,
+          });
 
-                    var data = {
-                        labels: chartjsLabel,
-                        datasets: [
-                            {
-                                fillColor: "rgba(151,187,205,0.5)",
-                                strokeColor: "rgba(151,187,205,0.8)",
-                                highlightFill: "rgba(151,187,205,0.75)",
-                                highlightStroke: "rgba(151,187,205,1)",
-                                data: chartjsData
-                            },
-                            {
-                                fillColor: "rgba(151,187,205,0.5)",
-                                strokeColor: "rgba(151,187,205,0.8)",
-                                highlightFill: "rgba(151,187,205,0.75)",
-                                highlightStroke: "rgba(151,187,205,1)",
-                                data: ratingData
-                            }
-                        ]
-                    };
-
-                    if ($(document).width() <= 800){
-                        var myBarChart = new Chart(ctx).Bar(data, {
-                            scaleLabel : "<%= Number(value) + ' F*'%>",
-                            scaleBeginAtZero: true,
-                            responsive: true
-                        });
-                    } else {
-                        var myBarChart = new Chart(ctx).Bar(data, {
-                            scaleLabel : "<%= Number(value) + ' F*'%>",
-                            scaleBeginAtZero: true,
-                        });
-                    }
-                }
-            })
         }
+      })
+    }
 
-    })
+  })
 });
 
 
--- script/linechart.js
+++ script/linechart.js
@@ -1,97 +1,136 @@
 $(document).ready(function() {
-    var BASE_URL = 'https://zebrastreifen.sifs0003.infs.ch/zebra';
-    
-    $.ajax({
-        url: `${BASE_URL}/api/v1/crossinglinechart`,
-        dataType: 'json',
-        method: 'get',
-        success: function (data1)  {
-            var ctx = document.getElementById("linecanvas").getContext("2d");
-            var dataSaved1 = data1;
-            var chartjsData = [];
-            for (var i = 0; i < dataSaved1.length; i++) {
-                chartjsData.push(dataSaved1[i].amount);
-            }
+  var BASE_URL = 'https://zebrastreifen.sifs0003.infs.ch/zebra';
+  var lineChartOptionsLogarithmic = {
+    responsive: true,
+    layout: {
+      padding: {
+        left: 20,
+        right: 20,
+        top: 0,
+        bottom: 0
+      }
+    },
+    title: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+      }],
+      yAxes: [{
+        display: true,
+        type: 'logarithmic',
+        ticks: {
+          callback: function(value, index, values) {
+            return Number(value).toString() + " F* ";
+          },
+        },
+      }]
+    },
+  };
+  var lineChartOptions = {
+    responsive: true,
+    layout: {
+      padding: {
+        left: 20,
+        right: 20,
+        top: 0,
+        bottom: 0
+      }
+    },
+    title: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+      }],
+      yAxes: [{
+        display: true,
+        ticks: {
+          callback: function(value, index, values) {
+            return Number(value).toString() + " F* ";
+          },
+        },
+      }]
+    },
+  };
 
-            var chartjsLabel = [];
-            for (var i = 0; i < dataSaved1.length; i++) {
-                chartjsLabel.push(dataSaved1[i].week);
-            }
-            var data = {
-                labels: chartjsLabel,
-                datasets: [
-                    {
-                        fillColor: "rgba(151,187,205,0.2)",
-                        strokeColor: "rgba(151,187,205,1)",
-                        pointColor: "rgba(151,187,205,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: chartjsData
-                    }
-                ]
-            };
-            if($(document).width() <= 800){
-                var myLineChart = new Chart(ctx).Line(data, {
-                    scaleLabel : "<%= Number(value) + ' F*'%>",
-                    //scaleBeginAtZero: true,
-                    responsive: true
-                });
-            } else {
-                var myLineChart = new Chart(ctx).Line(data, {
-                    scaleLabel : "<%= Number(value) + ' F*'%>",
-                   // scaleBeginAtZero: true
-                });
-            }
+  $.ajax({
+    url: `${BASE_URL}/api/v1/crossinglinechart`,
+    dataType: 'json',
+    method: 'get',
+    success: function (data1)  {
+      var ctx = document.getElementById("linecanvas").getContext("2d");
+      var dataSaved1 = data1;
+      var chartjsData = [];
+      for (var i = 0; i < dataSaved1.length; i++) {
+        chartjsData.push(dataSaved1[i].amount);
+      }
 
-        }
+      var chartjsLabel = [];
+      for (var i = 0; i < dataSaved1.length; i++) {
+        chartjsLabel.push(dataSaved1[i].week);
+      }
+      var data = {
+        labels: chartjsLabel,
+        datasets: [
+          {
+            label: "Fussgängerstreifen Total",
+            backgroundColor: "rgba(151,187,205,0.2)",
+            borderColor: "rgba(151,187,205,1)",
+            pointBackgroundColor: "rgba(151,187,205,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(151,187,205,1)",
+            data: chartjsData
+          }
+        ]
+      };
+      var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: lineChartOptionsLogarithmic,
+      });
+    }
+  });
+  $.ajax({
+    url: `${BASE_URL}/api/v1/ratinglinechart`,
+    dataType: 'json',
+    method: 'get',
+    success: function (data1)  {
+      var ctx = document.getElementById("ratinglinecanvas").getContext("2d");
+      var dataSaved1 = data1;
+      var chartjsData = [];
+      for (var i = 0; i < dataSaved1.length; i++) {
+        chartjsData.push(dataSaved1[i].amount);
+      }
 
-    });
-    $.ajax({
-        url: `${BASE_URL}/api/v1/ratinglinechart`,
-        dataType: 'json',
-        method: 'get',
-        success: function (data1)  {
-            var ctx = document.getElementById("ratinglinecanvas").getContext("2d");
-            var dataSaved1 = data1;
-            var chartjsData = [];
-            for (var i = 0; i < dataSaved1.length; i++) {
-                chartjsData.push(dataSaved1[i].amount);
-            }
-
-            var chartjsLabel = [];
-            for (var i = 0; i < dataSaved1.length; i++) {
-                chartjsLabel.push(dataSaved1[i].week);
-            }
-            var data = {
-                labels: chartjsLabel,
-                datasets: [
-                    {
-                        fillColor: "rgba(151,187,205,0.2)",
-                        strokeColor: "rgba(151,187,205,1)",
-                        pointColor: "rgba(151,187,205,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: chartjsData
-                    }
-                ]
-            };
-            if($(document).width() <= 800){
-                var LineChart = new Chart(ctx).Line(data, {
-                    scaleLabel : "<%= Number(value) + ' F*'%>",
-                    //scaleBeginAtZero: true,
-                    responsive: true
-                });
-            } else {
-                var LineChart = new Chart(ctx).Line(data, {
-                    scaleLabel : "<%= Number(value) + ' F*'%>",
-                    // scaleBeginAtZero: true
-                });
-            }
-
-        }
-
-    })
+      var chartjsLabel = [];
+      for (var i = 0; i < dataSaved1.length; i++) {
+        chartjsLabel.push(dataSaved1[i].week);
+      }
+      var data = {
+        labels: chartjsLabel,
+        datasets: [
+          {
+            label: "Bewertete Fussgängerstreifen",
+            backgroundColor: "rgba(151,187,205,0.2)",
+            borderColor: "rgba(151,187,205,1)",
+            pointBackgroundColor: "rgba(151,187,205,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(151,187,205,1)",
+            data: chartjsData
+          }
+        ]
+      };
+      var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: lineChartOptions,
+      });
+    }
+  })
 });
 
